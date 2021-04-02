fe3222936cb6e1dba7f957c7aec278e5f128ee3d
Anjali Pal <50593256+anjalipal2001@users.noreply.github.com>
2021-04-03 00:46:17 +0530

    Add files via upload

---
+++ build/precache-manifest.51ae21e057470640edabd5b84a49f1fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fd95111623c28989f19c594b1ec232",
+    "url": "/1proj/index.html"
+  },
+  {
+    "revision": "54314ca3c52bba2071f0",
+    "url": "/1proj/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "553c11c4cee074feae6d",
+    "url": "/1proj/static/css/main.45497a97.chunk.css"
+  },
+  {
+    "revision": "54314ca3c52bba2071f0",
+    "url": "/1proj/static/js/2.b6b179fc.chunk.js"
+  },
+  {
+    "revision": "2b9104cc996cacb095306a0d81fa5a1c",
+    "url": "/1proj/static/js/2.b6b179fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553c11c4cee074feae6d",
+    "url": "/1proj/static/js/main.0679321b.chunk.js"
+  },
+  {
+    "revision": "23bf05d320b7e4e4637b",
+    "url": "/1proj/static/js/runtime-main.4874482c.js"
+  },
+  {
+    "revision": "a6827cf5a483c13359d03d74eeb3951e",
+    "url": "/1proj/static/media/1.a6827cf5.jpg"
+  },
+  {
+    "revision": "b35d99509a21157f84888a170ddfbfed",
+    "url": "/1proj/static/media/2.b35d9950.jpg"
+  },
+  {
+    "revision": "5989ebbfe5acdbbce7d80b77164a9b0c",
+    "url": "/1proj/static/media/3.5989ebbf.jpg"
+  },
+  {
+    "revision": "5abb223434e6ae5ebfb6091d2ffe2db6",
+    "url": "/1proj/static/media/4.5abb2234.jpg"
+  },
+  {
+    "revision": "414026f730ac98c9ae5761c0efe6efa1",
+    "url": "/1proj/static/media/5.414026f7.jpg"
+  },
+  {
+    "revision": "54f2288fa1db1b0569dd4d74aa200172",
+    "url": "/1proj/static/media/card.54f2288f.jpg"
+  },
+  {
+    "revision": "84799a34e5bbc050be6e0f284ad7b26f",
+    "url": "/1proj/static/media/emptyBookmarks.84799a34.svg"
+  },
+  {
+    "revision": "93ba78358a09a1026c6cd1322a084502",
+    "url": "/1proj/static/media/emptyCart.93ba7835.svg"
+  },
+  {
+    "revision": "cd302fef4b563bd6e8951bef5effa8ee",
+    "url": "/1proj/static/media/icon.cd302fef.svg"
+  },
+  {
+    "revision": "159260d8a74b0cbad3439696b1da28f7",
+    "url": "/1proj/static/media/logo.159260d8.svg"
+  },
+  {
+    "revision": "b9dcccae818c5cbea8e792529ed61557",
+    "url": "/1proj/static/media/success.b9dcccae.svg"
+  }
+]);
